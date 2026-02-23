7a4a8ef3f6688a4983394224acb201d8280f1432
ranjeeth <ranji.reddy@gmail.com>
2026-02-23 20:45:15 +0530

    new upadte

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public" {
 }
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidr)
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.eks_vpc.id
   cidr_block = var.private_subnet_cidr[count.index]
   availability_zone = var.availability_zones[count.index]
 
